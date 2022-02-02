5bd0aa847627e73d557432052022026a6836cb43
s-muel <samuel.essuman@amalitech.org>
2022-02-02 16:56:41 +0000

    edited the event widget

--- lib/myevenpage.dart
+++ lib/myevenpage.dart
@@ -20,113 +20,113 @@ class MyEventPage extends StatelessWidget {
         body: ListView(
           children: [
             const SizedBox(
-              height: 30,
+              height: 40,
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                tileColor: Colors.white,
-                title: Text('Conference'),
-
-                trailing: const Image(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                    width: 150,
-                    fit: BoxFit.cover),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/event1');
-                // },
+            Card(
+              elevation: 10,
+              borderOnForeground: true,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("conference",
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                  Spacer(),
+                  Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                      width: 150,
+                      fit: BoxFit.cover),
+                ],
               ),
             ),
             const SizedBox(
-              height: 30,
+              height: 40,
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                tileColor: Colors.white,
-                title: Text('Conference'),
-
-                trailing: const Image(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                    width: 150,
-                    fit: BoxFit.cover),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/event1');
-                // },
+            Card(
+              elevation: 10,
+              borderOnForeground: true,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("conference",
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                  Spacer(),
+                  Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                      width: 150,
+                      fit: BoxFit.cover),
+                ],
               ),
             ),
             const SizedBox(
-              height: 30,
+              height: 40,
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                tileColor: Colors.white,
-                title: Text('Conference'),
-
-                trailing: const Image(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                    width: 150,
-                    fit: BoxFit.cover),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/event1');
-                // },
+            Card(
+              elevation: 10,
+              borderOnForeground: true,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("conference",
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                  Spacer(),
+                  Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                      width: 150,
+                      fit: BoxFit.cover),
+                ],
               ),
             ),
             const SizedBox(
-              height: 30,
+              height: 40,
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                tileColor: Colors.white,
-                title: Text('Conference'),
-
-                trailing: const Image(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                    width: 150,
-                    fit: BoxFit.cover),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/event1');
-                // },
+            Card(
+              elevation: 10,
+              borderOnForeground: true,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("conference",
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                  Spacer(),
+                  Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                      width: 150,
+                      fit: BoxFit.cover),
+                ],
               ),
             ),
             const SizedBox(
-              height: 30,
+              height: 40,
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                tileColor: Colors.white,
-                title: Text('Conference'),
-
-                trailing: const Image(
-                    image: NetworkImage(
-                        "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
-                    width: 150,
-                    fit: BoxFit.cover),
-                // onTap: () {
-                //   Navigator.pushNamed(context, '/event1');
-                // },
+            Card(
+              elevation: 10,
+              borderOnForeground: true,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("conference",
+                      style: TextStyle(fontSize: 20, color: Colors.black)),
+                  Spacer(),
+                  Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1591115765373-5207764f72e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
+                      width: 150,
+                      fit: BoxFit.cover),
+                ],
               ),
             ),
           ],
